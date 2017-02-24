f3bf0c0435dbd34e60396e342391f94bcfe3de66
baoyu <baiduxiaoqiang@126.com>
2017-02-24 23:17:50 +0800

    add utils

---
+++ AppLibrary/lib-utils/src/main/java/com/lib/utils/Utils.java
@@ -0,0 +1,52 @@
+package com.lib.utils;
+
+import android.content.Context;
+
+/**
+ * Author : Guazi
+ * Time: 17/2/24
+ * Desc : Utils初始化
+ */
+
+public class Utils {
+
+    private static Context context;
+
+    private static String shareName;
+
+    private Utils() {
+        throw new UnsupportedOperationException("Utils can't instantiate");
+    }
+
+    /**
+     * 初始化工具类
+     *
+     * @param context   上下文
+     * @param shareName SharedPreferences保存的名字
+     */
+    public static void init(Context context, String shareName) {
+        Utils.context = context;
+        Utils.shareName = shareName;
+    }
+
+    /**
+     * 获取上下文对象
+     *
+     * @return 上下文
+     */
+    public static Context getContext() {
+        if (context != null) {
+            return context;
+        }
+        throw new NullPointerException("Utils not init ");
+    }
+
+    /**
+     * 获取sp保存的名字
+     *
+     * @return
+     */
+    public static String getShareName() {
+        return shareName;
+    }
+}
