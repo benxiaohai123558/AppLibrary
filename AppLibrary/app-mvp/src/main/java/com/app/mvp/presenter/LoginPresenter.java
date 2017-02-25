7e64db9f4e544ad16e925d6ea3aae7b6ce73e8a6
liucj <guazi@2dfire.com>
2017-02-25 17:31:51 +0800

    d

--- AppLibrary/app-mvp/src/main/java/com/app/mvp/presenter/LoginPresenter.java
+++ AppLibrary/app-mvp/src/main/java/com/app/mvp/presenter/LoginPresenter.java
@@ -3,6 +3,7 @@ package com.app.mvp.presenter;
 import com.app.mvp.model.ILoginModel;
 import com.app.mvp.model.login.LoginModel;
 import com.app.mvp.view.ILoginView;
+import com.app.mvp.view.MainActivity;
 
 /**
  * author: Guazi.
@@ -13,19 +14,32 @@ import com.app.mvp.view.ILoginView;
 
 public class LoginPresenter {
 
+    private MainActivity activity;
+
     private ILoginView loginView;
 
     private ILoginModel loginModel;
 
-    public LoginPresenter(ILoginView loginView) {
+    public LoginPresenter(ILoginView loginView, MainActivity activity) {
         this.loginView = loginView;
+        this.activity = activity;
         loginModel = new LoginModel();
     }
 
-    public void login(String userName, String pwd) {
-        boolean result = loginModel.login(userName, pwd);
-        if (result) {
-            loginView.login();
-        }
+    public void login(final String userName, final String pwd) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                boolean result = loginModel.login(userName, pwd);
+                if (result) {
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            loginView.login();
+                        }
+                    });
+                }
+            }
+        }).start();
     }
 }
